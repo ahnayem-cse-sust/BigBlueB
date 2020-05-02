59891b29aa91359d12c4ee265a3d98f5cdd993f4
Nayem <ahnayem.sust@gmail.com>
2020-05-02 14:32:13 +0600

    rough

--- app/src/Controller/IndexController.php
+++ app/src/Controller/IndexController.php
@@ -8,10 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/bb", name="bb")
      */
     public function index()
     {
+        echo "gg";
+        exit();
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
